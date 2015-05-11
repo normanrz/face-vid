27d9079fe161467d5b614dea2c0e8c3953933be8
Tom Herold <tom.herold@scalableminds.com>
2015-05-11 15:36:15 +0200

    read video files, split into max k grey frames; fixes #5; fixes #3, fixes #1

--- optical_flow.py
+++ optical_flow.py
@@ -1,5 +1,5 @@
 from __future__ import generators
-import cv2, os, sys
+import cv2, os, sys, math
 import numpy as np
 
 CLASSIFIER_PATH = "/usr/local/Cellar/opencv/2.4.11/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml"
@@ -27,26 +27,53 @@ def detect_face(image):
 
 def preprocessMMI(image):
 
-    # crop the image to 360x576
-    return image[0:576, 360:720]
+    # turn into greyscale
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.equalizeHist(img)
 
-def face_pass(files):
+    # crop the image to 360x576
+    return img[0:576, 360:720]
+
+def read_video(video, max_frame_count):
+
+    # read video
+    frames = []
+    cap = cv2.VideoCapture(video)
+    frame_count = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+
+    # only grab & compute every x-th frame
+    stride = frame_count / float(max_frame_count)
+    relevant_frames = [int(i) for i in np.arange(0, frame_count, stride)]
+
+    if cap.isOpened():
+
+        for i in range(0, frame_count):
+
+            if not i in relevant_frames:
+                # skip video frame without decoding
+                cap.grab()
+                continue
+            else:
+                # actually read a frame
+                returnValue, frame = cap.read()
+
+            if not returnValue:
+                break
+
+            image = preprocessMMI(frame)
+            frames.append(image)
+
+        cap.release()
+        return frames
+    else:
+        sys.exit("Error opening video file.")
+
+def face_pass(images):
 
     minX = minY = sys.maxint
     maxWidth = maxHeight = 0
-    images = []
 
-    # for file in files:
-    cap = cv2.VideoCapture(files)
-    while cap.isOpened():
-
-        returnValue, img = cap.read()
-
-        if not returnValue:
-            break
-
-        # special case MMI dataset
-        image = preprocessMMI(img)
+    for image in images:
 
         # image = cv2.imread(file)
         (x, y, w, h) = detect_face(image)
@@ -56,9 +83,6 @@ def face_pass(files):
         maxWidth = max(maxWidth, w)
         maxHeight = max(maxHeight, h)
 
-        images.append(image)
-
-    cap.release()
 
     for image in images:
 
@@ -71,6 +95,7 @@ def face_pass(files):
         mask = np.zeros_like(cropped_image)
         # (cropped_image, center, axes, angle, startAngle, endAngle, color[, thickness[, lineType[, shift]]])  None
         cv2.ellipse(mask, center, axes, 0, 0, 360, (255, 255, 255), -1)
+
         yield np.bitwise_and(cropped_image, mask)
 
 
@@ -78,23 +103,15 @@ def flow_pass(images):
 
 
     prev = images.next()
-    hsv = np.zeros_like(prev)
-    hsv[..., 1] = 255
 
-    prev = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
-    cv2.equalizeHist(prev)
-
-    for image in images:
-
-        next = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cv2.equalizeHist(next)
+    for next in images:
 
         # prev, next, pyr_scale, levels, winsize, iterations, poly_n, poly_sigma, flags[, flow]
         flow = cv2.calcOpticalFlowFarneback(prev, next,  0.5,  3,  15,  3,  2,  1.1,  0)
 
         cv2.imshow('flow x', flow[..., 0])
         cv2.imshow('flow y', flow[..., 1])
-        cv2.imshow('image ', image)
+        cv2.imshow('image ', next)
 
         # Wait indefinitely for user input
         k = cv2.waitKey(0) & 0xff
@@ -112,18 +129,19 @@ def flow_pass(images):
 
 def main():
 
-    if len(sys.argv) < 2:
-        sys.exit("Usage: %s <path_to_video>" % sys.argv[0])
+    if len(sys.argv) < 3:
+        sys.exit("Usage: %s <max_frame_count> <path_to_video>" % sys.argv[0])
 
     # read path to image as command argument
-    video_path = os.path.abspath(sys.argv[1])
+    max_frame_count = int(sys.argv[1])
+    video_path = os.path.abspath(sys.argv[2])
 
     if not os.path.isfile(video_path):
         sys.exit("The specified argument is not a valid filename")
 
     # ready to rumble
-    #image_files = list_files(video_path)
-    images = face_pass(video_path)
+    frames = read_video(video_path, max_frame_count)
+    images = face_pass(frames)
     flow_pass(images)
 
 
