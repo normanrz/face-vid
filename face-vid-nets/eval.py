c4ca496b86063a717a92eaddd7092a9c491204c6
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-06-02 11:34:47 +0200

    added evalutation and improved deploy.txt

--- face-vid-nets/eval.py
+++ face-vid-nets/eval.py
@@ -6,6 +6,32 @@ import lmdb
 import argparse
 from collections import defaultdict
 
+# Pretty print confusion table of predicted vs true labels
+def print_table(headers, data):
+    names = list(headers)
+    row_format ="{:>6}" + "{:>5}" * len(names) + "{:>6}"
+    print row_format.format("", *(names + ["[sum]"]))
+    predictedSums = defaultdict(int)
+    for l in names:
+        row = []
+        trueSum = 0
+        for pl in names:
+            if l == pl:
+                row.append("(%d)" %matrix[(l,pl)])
+            else:
+                row.append(matrix[(l,pl)])
+            trueSum += matrix[(l,pl)]
+            predictedSums[pl] += matrix[(l,pl)]
+        row.append(trueSum)
+        print row_format.format(l, *row)
+
+    row = []
+    for pl in names:
+        row.append(predictedSums[pl])
+    print row_format.format("[sum]", *(row + [sum(row)]))
+
+# Example:
+# python eval.py --proto an-finetune/deploy.prototxt --model snapshots/_iter_50000.caffemodel --lmdb MMI_OAO_TEST#
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--proto', type=str, required=True)
@@ -24,14 +50,15 @@ if __name__ == "__main__":
     lmdb_txn = lmdb_env.begin()
     lmdb_cursor = lmdb_txn.cursor()
 
-    for key, value in lmdb_cursor:
+    # iterate through lmdb and test all datums
+    for key, value in  lmdb_cursor:
         datum = caffe.proto.caffe_pb2.Datum()
         datum.ParseFromString(value)
         label = int(datum.label)
         image = caffe.io.datum_to_array(datum)
         image = image.astype(np.uint8)
 
-        out = net.forward_all(data=np.asarray([image]))
+        out = net.forward_all(data=np.asarray([image])[:, :, 0:227, 0:227])
         plabel = int(out['prob'][0].argmax(axis=0))
 
         count = count + 1
@@ -51,7 +78,6 @@ if __name__ == "__main__":
 
     print ""
     print "Confusion matrix:"
-    print "(r , p) | count"
-    for l in labels_set:
-        for pl in labels_set:
-            print "(%i , %i) | %i" % (l, pl, matrix[(l,pl)])
+    print "true label vs predicted label"
+
+    print_table(labels_set, matrix)
