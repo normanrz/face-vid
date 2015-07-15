b8bc9286a80e93e426f9b85a5c14e4f409774cc1
Norman Rzepka <code@normanrzepka.de>
2015-07-15 18:06:18 +0200

    Added row counting to count_labels.py

--- model-evaluation/count_labels.py
+++ model-evaluation/count_labels.py
@@ -10,16 +10,19 @@ if __name__ == "__main__":
 
   hdf5list = sys.argv[1]
   output = np.zeros(NUMBER_OF_LABELS, dtype="int")
+  nrow = 0
 
   with open(hdf5list, 'r') as f:
     for hdf5File in f:
       hdf5File = hdf5File.rstrip()
       if not hdf5File: continue
-      # read hdf5 and test all examples
-      print "About to load: %s" % hdf5File
+      # read hdf5 and count all labels
+      print hdf5File
       h5file = h5py.File(hdf5File)
       labels = h5file['label'][...].astype(bool)
 
       output = output + np.sum(labels, axis=0)
+      nrow = nrow + labels.shape[0]
 
   print output
+  print nrow
